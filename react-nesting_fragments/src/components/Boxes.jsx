cbfaa6042ef2482fe6174479a09a5d32d6bccc45
Daniel <cyubahirodaniel@gmail.com>
2025-03-26 15:39:40 +0100

    react-nesting_fragments

---
+++ react-nesting_fragments/src/components/Boxes.jsx
@@ -0,0 +1,13 @@
+import Box from './Box.jsx';
+import React from 'react';
+
+const Boxes = () => {
+  return (
+      <>
+        <Box color="#007bff"/>
+        <Box color="#fc3"/>
+        <Box color="#ff3333"/>
+      </>
+  );
+};
+export default Boxes;
